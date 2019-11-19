f1863869592ed41d139c783c1eae2371996ac67f
Anubhav Marwaha <anubhavmarwaha2000@gmail.com>
2019-11-19 23:49:46 +0530

    CPU code working. Draw function to be made GPU compatibility next on list
    Signed-off-by: Anubhav Marwaha <anubhavmarwaha2000@gmail.com>

--- cfg.py
+++ cfg.py
@@ -5,7 +5,7 @@ class d():
         self.noise_dim = 100
         self.emb_dim = 1024
         self.projected_em_dim = 128
-        self.batch_size = 64
+        self.batch_size = 429
         self.learning_rate = 0.0002
         self.epochs = 200
         self.cuda = True
